dacecc66b56c2205fd245697201a2f6daba71484
skygoose <chrisgu99@outlook.com>
2025-07-05 23:12:49 +1000

    :hammer: Fixed brew timer

--- src/timer.py
+++ src/timer.py
@@ -10,20 +10,22 @@ def timer(
         brew: Brew,
         interval: Optional[int] = 45
     ):
+    print(brew.get_num_pours())
     total_time: int = interval * brew.get_num_pours()
-    print(brew_1.get_info())
-    print(f"Total seconds is {total_time}")
-
+    total_minutes: int = total_time // 60
+    total_seconds: int = total_time % 60
+    print(brew.get_info())
+    print(f"Total brew time to last pour is {total_minutes}:{total_seconds}")
     try:
         for i in range(total_time):
-            minutes = i // 60
-            seconds = i % 60
+            minutes, seconds = divmod(i, 60)
             time.sleep(1)
-            if i % interval == 0:
-                pour_num: int = i // interval + 1
-                sys.stdout.write(f" Time to do pour #{pour_num}")
+            pour_num: int = i // interval + 1
+            pour_to_weight: int = pour_num * brew.get_coffee_weight() * brew.get_water_ratio()[1] / brew.get_water_ratio()[0] / brew.num_pours
             timer_display:str = f"{minutes:02d}:{seconds:02d}"
-            sys.stdout.write(f"\rTime elapsed: {timer_display}")
+            sys.stdout.write(f"\rPour #{pour_num} until {pour_to_weight}g - {timer_display}")
+            sys.stdout.flush()
+        print("You have finished your brew! Enjoy :)")
     except KeyboardInterrupt:
         print("\nTimer stopped.")
 
@@ -32,6 +34,7 @@ def timer(
 
 
 if __name__ == "__main__":
+    # Test Brew
     brew_1 = Brew(beans="Ignis Bourbon")
     timer(brew_1)
 
